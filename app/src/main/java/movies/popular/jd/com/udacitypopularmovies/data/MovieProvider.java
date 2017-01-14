392ee44c177c1028dd67142695415032dd46621c
chuongdao <cd.kernel.dev.105@gmail.com>
2017-01-14 09:06:42 -0800

    Finish Bulk Insert

--- app/src/main/java/movies/popular/jd/com/udacitypopularmovies/data/MovieProvider.java
+++ app/src/main/java/movies/popular/jd/com/udacitypopularmovies/data/MovieProvider.java
@@ -2,7 +2,9 @@ package movies.popular.jd.com.udacitypopularmovies.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -12,21 +14,89 @@ import android.support.annotation.Nullable;
 
 public class MovieProvider  extends ContentProvider{
 
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private  MovieDBHelper mMovieDBHelper;
+    // URI ID
+    static final int MOVIES = 400;
+    static final int MOVIES_FAVORITE = 403;
+    static final int MOVIES_HIGHEST_RATED = 401;
+    static final int MOVIES_MOST_POPULAR = 406;
+    static final int MOVIE_SPECIFIC_TRAILERS = 402;
+    static final int MOVIE_SPECIFIC_REVIEWS = 405;
+    static final int MOVIE_ITEM_INFO = 404;
+
+
     @Override
     public boolean onCreate() {
-        return false;
+        //create new db
+        mMovieDBHelper = new MovieDBHelper(getContext());
+        return true;
     }
 
-    @Nullable
-    @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
-        return null;
-    }
 
+
+    /**
+     * deteremine the URI type here
+     * @param uri
+     * @return
+     */
     @Nullable
     @Override
     public String getType(Uri uri) {
-        return null;
+
+        final int match = sUriMatcher.match(uri);
+
+        switch (match){
+            case MOVIES:
+                return MovieContract.MovieEntry.CONTENT_TYPE;
+            case MOVIE_ITEM_INFO:
+                return MovieContract.MovieEntry.CONTENT_ITEM_TYPE;
+            case MOVIE_SPECIFIC_TRAILERS:
+                return MovieContract.TrailerEntry.CONTENT_TYPE;
+            case MOVIE_SPECIFIC_REVIEWS:
+                return MovieContract.ReviewEntry.CONTENT_TYPE;
+            case MOVIES_HIGHEST_RATED:
+                return MovieContract.MovieEntry.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    /**
+     * insert on ContentValues to DB in one transaction which proves to be
+     * more efficient.
+     * @param uri
+     * @param values
+     * @return
+     */
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+
+        final SQLiteDatabase db = mMovieDBHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+
+        // match case to insert data
+        switch (match){
+            case MOVIES:
+                int totalInserted = 0;
+                db.beginTransaction();
+                try {
+                    for (ContentValues val : values){
+                        long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME,null,val);
+                        if (_id != -1){
+                            totalInserted++;
+                        }
+                    }
+                    // done
+                    db.setTransactionSuccessful();
+                } finally {
+                    // end transaction to keep data consistent
+                    db.endTransaction();
+                }
+                return totalInserted;
+            default:
+                return super.bulkInsert(uri, values);
+        }
     }
 
     @Nullable
@@ -43,5 +113,43 @@ public class MovieProvider  extends ContentProvider{
     @Override
     public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
         return 0;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    static UriMatcher buildUriMatcher(){
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY,
+                                                MovieContract.PATH_MOVIES, MOVIES);
+
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY,
+                                    MovieContract.PATH_FAVORITE_MOVIES, MOVIES_FAVORITE);
+
+        // used to get either trailers or reviews
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY,
+                                MovieContract.PATH_MOVIES +"/#/trailers",
+                MOVIE_SPECIFIC_TRAILERS);
+
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY,
+                MovieContract.PATH_MOVIES +"/#/reviews",
+                MOVIE_SPECIFIC_REVIEWS);
+
+        // uri used to get movies specific info.
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY,
+                MovieContract.PATH_MOVIES +"/#", MOVIE_ITEM_INFO);
+
+        // uri used to get movies specific info.
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY,
+                MovieContract.PATH_MOVIES +"/highest_rated", MOVIES_HIGHEST_RATED);
+
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY,
+                MovieContract.PATH_MOVIES +"/most_popular", MOVIES_MOST_POPULAR);
+
+        return matcher;
     }
 }
